c3afb2b7ff9882f08a55ee5e351e0e7ba66c79c5
SHETER <47436766+SHETER0@users.noreply.github.com>
2022-10-24 11:17:19 +0300

    Add files via upload

---
+++ public/electron.js
@@ -0,0 +1,80 @@
+const { app, BrowserWindow, ipcMain } = require('electron'); // electron
+const isDev = require('electron-is-dev'); // To check if electron is in development mode
+const path = require('path');
+
+let mainWindow;
+
+// Initializing the Electron Window
+const createWindow = () => {
+  mainWindow = new BrowserWindow({
+    width: 600, // width of window
+    height: 600, // height of window
+    webPreferences: {
+      // The preload file where we will perform our app communication
+      preload: isDev 
+        ? path.join(app.getAppPath(), './public/preload.js') // Loading it from the public folder for dev
+        : path.join(app.getAppPath(), './build/preload.js'), // Loading it from the build folder for production
+      worldSafeExecuteJavaScript: true, // If you're using Electron 12+, this should be enabled by default and does not need to be added here.
+      contextIsolation: true, // Isolating context so our app is not exposed to random javascript executions making it safer.
+    },
+  });
+
+	// Loading a webpage inside the electron window we just created
+  mainWindow.loadURL(
+    isDev
+      ? 'http://localhost:3000' // Loading localhost if dev mode
+      : `file://${path.join(__dirname, '../build/index.html')}` // Loading build file if in production
+  );
+
+	// Setting Window Icon - Asset file needs to be in the public/images folder.
+  mainWindow.setIcon(path.join(__dirname, 'images/appicon.ico'));
+
+	// In development mode, if the window has loaded, then load the dev tools.
+  if (isDev) {
+    mainWindow.webContents.on('did-frame-finish-load', () => {
+      mainWindow.webContents.openDevTools({ mode: 'detach' });
+    });
+  }
+};
+
+// ((OPTIONAL)) Setting the location for the userdata folder created by an Electron app. It default to the AppData folder if you don't set it.
+app.setPath(
+  'userData',
+  isDev
+    ? path.join(app.getAppPath(), 'userdata/') // In development it creates the userdata folder where package.json is
+    : path.join(process.resourcesPath, 'userdata/') // In production it creates userdata folder in the resources folder
+);
+
+// When the app is ready to load
+app.whenReady().then(async () => {
+   createWindow(); // Create the mainWindow
+
+
+});
+
+// Exiting the app
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+// Activating the app
+app.on('activate', () => {
+  if (mainWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
+
+// Logging any exceptions
+process.on('uncaughtException', (error) => {
+  console.log(`Exception: ${error}`);
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+  //IPC MAIN
+  ipcMain.on("test-send", (event, args) => {
+    console.log(args)
+  })
